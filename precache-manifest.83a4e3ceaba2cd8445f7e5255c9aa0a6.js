35bd91f4d62d95c63ce5a9303502d09b87cf2632
NinjaEagle <kcwang06@gmail.com>
2019-11-18 19:15:35 -0500

    Updates

---
+++ precache-manifest.83a4e3ceaba2cd8445f7e5255c9aa0a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b43bbc2a629eea2ad248acbd8b978fe",
+    "url": "/kevinwang/index.html"
+  },
+  {
+    "revision": "f5a682844927db7e193c",
+    "url": "/kevinwang/static/css/main.db3fdc95.chunk.css"
+  },
+  {
+    "revision": "87bf2d121b1688f4ca7f",
+    "url": "/kevinwang/static/js/2.36498ff7.chunk.js"
+  },
+  {
+    "revision": "f5a682844927db7e193c",
+    "url": "/kevinwang/static/js/main.c33dfdc7.chunk.js"
+  },
+  {
+    "revision": "83b87a9e5725967e4a51",
+    "url": "/kevinwang/static/js/runtime-main.788ec012.js"
+  },
+  {
+    "revision": "45e46bc025b8fd350c09492ce01d4549",
+    "url": "/kevinwang/static/media/Postgresql.45e46bc0.svg"
+  },
+  {
+    "revision": "1fc89f552fd5ed8cf7069fd360cee8e8",
+    "url": "/kevinwang/static/media/kevin.1fc89f55.jpg"
+  },
+  {
+    "revision": "fcbfd0a14f165130fefe785260231083",
+    "url": "/kevinwang/static/media/nyc.fcbfd0a1.jpg"
+  },
+  {
+    "revision": "8614e1a4b4b46891877d6ad39768d6b6",
+    "url": "/kevinwang/static/media/rails.8614e1a4.png"
+  },
+  {
+    "revision": "fd9ab9ee672ba92de7d809b53dbb6bdd",
+    "url": "/kevinwang/static/media/redux.fd9ab9ee.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kevinw-website/precache-manifest.a759cc5104a0274df8ca3e031b93b7e1.js"
+  "/kevinwang/precache-manifest.83a4e3ceaba2cd8445f7e5255c9aa0a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kevinw-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kevinwang/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
