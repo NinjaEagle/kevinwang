35df2edc5d419309b5ba59d220f3fa8bf7ee4be6
Kevin Wang <kcwang06@gmail.com>
2021-01-29 11:47:08 -0500

    add hackernews clone detailse into projects

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 export default function MyInfo() {
 	return (
@@ -8,10 +8,12 @@ export default function MyInfo() {
 					<a
 						href='mailto: kcwang06@gmail.com'
 						target='_blank'
-						rel='noopener noreferrer'>
+						rel='noopener noreferrer'
+					>
 						<i
 							className='contactlinks fas fa-envelope-square fa-3x wow bounceIn'
-							data-wow-delay='.1s'></i>
+							data-wow-delay='.1s'
+						></i>
 					</a>
 				</div>
 
@@ -19,40 +21,48 @@ export default function MyInfo() {
 					<a
 						href='https://github.com/NinjaEagle'
 						rel='noopener noreferrer'
-						target='_blank'>
+						target='_blank'
+					>
 						<i
 							className='contactlinks fab fa-github-square fa-3x wow bounceIn'
-							aria-hidden='true'></i>
+							aria-hidden='true'
+						></i>
 					</a>
 				</div>
 				<div>
 					<a
 						href='https://www.linkedin.com/in/kevin-wang-06/'
 						rel='noopener noreferrer'
-						target='_blank'>
+						target='_blank'
+					>
 						<i
 							className='contactlinks fab fa-linkedin fa-3x wow bounceIn'
-							aria-hidden='true'></i>
+							aria-hidden='true'
+						></i>
 					</a>
 				</div>
 				<div>
 					<a
 						href='https://twitter.com/kevlyoko'
 						rel='noopener noreferrer'
-						target='_blank'>
+						target='_blank'
+					>
 						<i
 							className='contactlinks fab fa-twitter fa-3x wow bounceIn'
-							aria-hidden='true'></i>
+							aria-hidden='true'
+						></i>
 					</a>
 				</div>
 				<div>
 					<a
 						href='https://medium.com/@kwang06'
 						rel='noopener noreferrer'
-						target='_blank'>
+						target='_blank'
+					>
 						<i
 							className='contactlinks fab fa-medium fa-3x wow bounceIn'
-							aria-hidden='true'></i>
+							aria-hidden='true'
+						></i>
 					</a>
 				</div>
 			</div>
@@ -62,11 +72,12 @@ export default function MyInfo() {
 				<a
 					href='https://docs.google.com/document/d/1bKWJr-YGZ-OuUAfWtDJYiyBT9Rxj0Z4VX0N--u4w7x0/edit?usp=sharing'
 					rel='noopener noreferrer'
-					target='_blank'>
+					target='_blank'
+				>
 					<i className=''></i>
 					My Resume
 				</a>
 			</div>
 		</Fragment>
-	)
+	);
 }
